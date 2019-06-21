8b4eafe979891a7c4597e6068a79eff5781933e8
abdallah-khalil <abdallah.khalil.nada@gmail.com>
2019-06-21 06:47:55 +0200

    adding clarity design system fixing the selector in global ts lint file

--- apps/demo-prio/src/app/app.module.ts
+++ apps/demo-prio/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    ClarityModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
