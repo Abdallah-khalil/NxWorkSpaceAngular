b9f536abf5d48843920739bee5a79e8334b34f56
abdallah-khalil <abdallah.khalil.nada@gmail.com>
2019-07-03 13:23:14 +0200

    remove unnecessary library

--- apps/demo-prio/src/app/app.module.ts
+++ apps/demo-prio/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
